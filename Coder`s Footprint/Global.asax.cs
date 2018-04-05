fa514c6130fff304ba502279e1044331a0ca44ed
ALEXANDRU-CROIT\alexc <alexandru.croitoru@softstatetechnologies.com>
2018-04-05 14:35:06 +0300

    Removed unnecessary code from Global.asax.cs regarding HTTPS and modified Web.config for forcing HTTPS.

--- Coder`s Footprint/Global.asax.cs
+++ Coder`s Footprint/Global.asax.cs
@@ -19,13 +19,5 @@ namespace Coder_s_Footprint
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-
-        protected void Application_BeginRequest(Object sender, EventArgs e)
-        {
-            if (HttpContext.Current.Request.IsSecureConnection.Equals(false) && HttpContext.Current.Request.IsLocal.Equals(false))
-            {
-                Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"]+HttpContext.Current.Request.RawUrl);
-            }
-        }
     }
 }
